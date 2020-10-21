41b3a2888cc1f3ff7be0411003c33077248d03ea
peteawood <pete.a.wood@gmail.com>
2020-10-21 14:14:18 +0100

    Update main.go

--- main.go
+++ main.go
@@ -35,7 +35,7 @@ func main() {
 
 		io.WriteString(w, string(out))
 
-		fmt.Println("Hello world - the log message")
+		fmt.Println("Hello world - resolving 3rd party cookies FTW")
 	})
 	http.ListenAndServe(":8080", nil)
 }
